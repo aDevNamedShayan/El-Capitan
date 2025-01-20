6b102f3139e3122d4d907b5a971b99d02d730cde
Shayan <nwryshayan500@gmail.com>
2025-01-20 20:24:10 +0330

    Added the Corvete Models

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber"
 import { Suspense } from "react"
+import { Ground } from "./Ground";
 
-function CarShow() {
+const CarShow = () => {
   return (
     <>
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
 
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
 
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial color={'red'} />
-      </mesh>
+      <color args={[0, 0, 0]} attach='background' />
+
+      <spotLight 
+        color={[1, 0.25, 0.7]}
+        intensity={1500}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      
+      <spotLight 
+        color={[0.14, 0.5, 1]}
+        intensity={2000}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <Ground />
     </>
   )
 }
 
-function App() {
+const App = () => {
   return (
     <Suspense fallback={null}>
       <Canvas shadows>
